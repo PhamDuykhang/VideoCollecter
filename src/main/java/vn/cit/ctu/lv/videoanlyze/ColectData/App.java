b964e1948ed26bf2dd5af44fc8adff7be6234e36
PhamDuyKhang <khangy9a1@gmail.com>
2018-06-23 09:32:42 +0700

    init(Khởi tạo dự án)

---
+++ src/main/java/vn/cit/ctu/lv/videoanlyze/ColectData/App.java
@@ -0,0 +1,13 @@
+package vn.cit.ctu.lv.videoanlyze.ColectData;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
